75b66bf9476e99078257a2b9db7433a6e796cd8f
goreleaserbot <goreleaser@carlosbecker.com>
2020-10-30 12:40:11 +0000

    Brew formula update for jenkins-credentials-decryptor version 1.1.1

--- Formula/jenkins-credentials-decryptor.rb
+++ Formula/jenkins-credentials-decryptor.rb
@@ -2,16 +2,16 @@
 class JenkinsCredentialsDecryptor < Formula
   desc "Navigitae locally cloned repos."
   homepage "https://github.com/hoto/jenkins-credentials-decryptor"
-  version "1.1.0"
+  version "1.1.1"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/hoto/jenkins-credentials-decryptor/releases/download/1.1.0/jenkins-credentials-decryptor_Darwin_x86_64.zip"
-    sha256 "15e18b06720c468ade91feb83c2a61b64cbaf59969cd457e4cecd99f6e008a26"
+    url "https://github.com/hoto/jenkins-credentials-decryptor/releases/download/1.1.1/jenkins-credentials-decryptor_Darwin_x86_64.zip"
+    sha256 "eaadff8c7faa9cdd846ce808b8aaa2e2b9b82c308c6ecc73a62a61dd634ccfc5"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/hoto/jenkins-credentials-decryptor/releases/download/1.1.0/jenkins-credentials-decryptor_Linux_x86_64.zip"
-      sha256 "fa0ef061ead425d89484f367e813d77400bffb725664f92057b7da6a132797bf"
+      url "https://github.com/hoto/jenkins-credentials-decryptor/releases/download/1.1.1/jenkins-credentials-decryptor_Linux_x86_64.zip"
+      sha256 "ff66db042a8906f55ba89ec26e929fe79f586ddcd0bdbe4d36d28e302090a08b"
     end
   end
 
